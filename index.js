2abf90ae3063c02ee7fcfc50d6e2708c81390c66
Marcelo Souza Santos <marcelo.santos@virtus.ufcg.edu.br>
2022-06-11 18:44:16 -0300

    add-base-protocol - add base SOAP protocol to node project

---
+++ index.js
@@ -0,0 +1,28 @@
+const soap = require('soap');
+const url = 'https://www.crcind.com/csp/samples/SOAP.Demo.CLS?WSDL';
+
+soap.createClient(url, (err, client) => {
+    if (err) {
+        console.error("An error occurred creating client:", err);
+        return;
+    }
+    execution(client, 'AddInteger', {Arg1: 2, Arg2: 2});
+    execution(client, 'DivideInteger', {Arg1: 2, Arg2: 2});
+    execution(client, 'FindPerson', {id: 1});
+    execution(client, 'GetByName', {name: 'Newton,Dave R.'});
+    execution(client, 'GetDataSetByName', {name: 'Test'});
+    execution(client, 'GetListByName', {name: 'Test'});
+    // execution(client, 'LookupCity', {zip: 63163});
+    execution(client, 'Mission', {});
+    execution(client, 'QueryByName', {name: 'Test'});
+});
+
+function execution(client, nameFunction, args) {
+    return client[nameFunction](args, (err, response) => {
+        if (err) {
+            console.error(`An error occurred calling client.${nameFunction}: `, err);
+            return;
+        }
+        console.log(`client.${nameFunction}: response: `, response);
+    });
+}
